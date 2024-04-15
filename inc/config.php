0031da5b60a830d596075af1348799deeb6e22d9
Afam Ifeanyi(Chrys) M <afam_ifeanyi@live.com>
2024-04-15 17:06:17 +0300

    Update config.php

--- inc/config.php
+++ inc/config.php
@@ -1,6 +1,6 @@
 <?php 
-define('DB_HOST', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'chris414');
+define('DB_HOST', 'product-db-1.mysql.database.azure.com'); 
+define('DB_USERNAME', 'product_');
+define('DB_PASSWORD', 'chris414#');
 define('DB_DATABASE_NAME', 'products_api');
 ?>
